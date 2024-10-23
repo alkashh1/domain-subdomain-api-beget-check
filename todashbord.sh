5c7bcb789d1e4a09e4054c7db238b694b70e402b
Arabik <alkashh1@yandex.ru>
2024-10-23 17:54:29 +0300

    новый дашборд

--- todashbord.sh
+++ todashbord.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-cat /home/zabbix/scripts/DomainTree/domain_tree.txt
+
+# Скрипт для отправки содержимого файла в Zabbix
+ZABBIX_SERVER="127.0.0.1:10050"  # Замените на IP или имя вашего Zabbix сервера
+HOSTNAME="vpn-server6"  # Имя хоста в Zabbix
+ITEM_KEY="domain.subdomain.fromfile"  # Уникальный ключ элемента данных
+FILE_PATH="/home/zabbix/scripts/DomainTree/domain_tree.txt"  # Путь к вашему файлу
+
+# Чтение файла
+FILE_CONTENT=$(cat "$FILE_PATH")
+
+# Отправка содержимого файла в Zabbix
+zabbix_sender -z "$ZABBIX_SERVER" -s "$HOSTNAME" -k "$ITEM_KEY" -o "$FILE_CONTENT"
